f404c1270c48fdcd8cd8f512c94b7f7b09d52750
Meezan Shaikh <heymeezan@gmail.com>
2025-07-24 17:03:58 +0530

    incorrect-author-name

--- components/Header.tsx
+++ components/Header.tsx
@@ -11,7 +11,7 @@ export default function Header() {
   return (
     <header className="sticky top-0 z-50 backdrop-blur-md bg-[#F7E1D7] shadow-sm border-b border-gray-200">
       <div className="max-w-6xl mx-auto px-4 py-4 flex justify-between items-center">
-        {/* Logo */}
+     
       <div className="flex items-center gap-3">
   <Image
     src={"/images/dishes/crave-bots.png"}
